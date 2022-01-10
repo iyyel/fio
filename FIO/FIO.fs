9095157f27d0a8fcc8f9d10edd6697655fcb429c
iyyel <mail@iyyel.io>
2022-01-10 21:26:24 +0100

    Simple working solution using a queue.

--- FIO/FIO.fs
+++ FIO/FIO.fs
@@ -1,58 +1,39 @@
 ﻿namespace FSharp.FIO
 
-open System.Net.Sockets
-open System.Net
-open System
+open System.Collections.Generic
 
 module FIO =
-
-    let addr = "127.0.0.1"
-    let port = 8888
+    
+    let defaultChannel = Queue<'a>()
 
     type Effect<'a> =
         | Input of ('a -> Effect<'a>)
-        | Output of 'a
+        | Output of 'a * (unit -> Effect<'a>)
         | Parallel of Effect<'a> * Effect<'a>
+        | Unit
 
-    let send v = Output v
-    let receive (f: 'a -> Effect<'a>) = Input(f)
+    let send (v, f) = Output(v, f)
+    let receive f = Input f
     
-    let sendToSocket addr port (msg : string) =
-        try
-            let tcpClient = new TcpClient(addr, port)
-            let data = System.Text.Encoding.ASCII.GetBytes msg
-            let networkStream = tcpClient.GetStream()
-            networkStream.Write(data, 0, Array.length data)
-            printfn "Sent message: %s" msg
-            networkStream.Close()
-            networkStream.Dispose()
-        with
-        | :? ArgumentNullException as excp ->
-            printfn "ArgumentNullException encountered: %s" excp.Message
-        | :? SocketException as excp ->
-            printfn "SocketException encountered: %s" excp.Message
-
-    let receiveFromSocket (addr:String) port = 
-        async {
-            let addr' = IPAddress.Parse(addr)
-            let tcpListener = new TcpListener(addr', port)
-            tcpListener.Start()
-            let bytes = Array.create 256 (byte (0))
-            printfn "Waiting for a connection..."
-            let tcpClient = tcpListener.AcceptTcpClient()
-            let ns = tcpClient.GetStream()
-            let mutable i = ns.Read(bytes, 0, (Array.length bytes))
-            let data = System.Text.Encoding.ASCII.GetString(bytes, 0, i)
-            printfn "Received: %s" data
-            tcpListener.Stop()
-            return data
-        }
+    let sendToChannel (c : Queue<'a>) v =
+        c.Enqueue v
+    
+    let receiveFromChannel (c : Queue<'a>) =
+        if c.Count = 0 then
+            "error: channel is empty!"
+        else
+            c.Dequeue()
         
     let rec naiveEval e = 
         match e with 
-        | Input(f)         -> let v = receiveFromSocket addr port |> Async.RunSynchronously
-                              printfn "Received message '%s' from %s:%d" v addr port
+        | Input f          -> let v = receiveFromChannel defaultChannel
+                              printfn "Received message: '%s'" v
                               naiveEval(f v)
-        | Output(v)        -> sendToSocket addr port v
-                              printfn "Sent message '%s' to %s:%d" v addr port
-        | Parallel(e1, e2) -> failwith "not implemented!"
+        | Output(v, f)     -> sendToChannel defaultChannel v
+                              printfn "Sent message: '%s'" v
+                              naiveEval(f ())
+        | Parallel(e1, e2) -> async {
+                                naiveEval(e1)
+                              } |> Async.Start
+                              naiveEval(e2)
+        | Unit             -> ()
--- examples/Examples.fs
+++ examples/Examples.fs
@@ -1,25 +1,28 @@
 ﻿open FSharp.FIO
 
+let p: FIO.Effect<string> =
+    FIO.send("test 1", fun () ->
+        FIO.send("test 2", fun () -> 
+            FIO.receive(fun x -> 
+                FIO.receive(
+                fun y -> 
+                    FIO.send(x + " " + y, fun () ->
+                        FIO.receive(fun y -> FIO.Unit))))))
+
+let p1: FIO.Effect<string> =
+    FIO.receive(fun x -> 
+        FIO.receive (fun y -> 
+            FIO.send(x + y, fun () -> FIO.Unit)))
+
+let p2: FIO.Effect<string> = FIO.Parallel(
+    FIO.send("message 1", fun () -> FIO.receive (fun x -> FIO.Unit)),
+    FIO.send("message 2", fun () -> FIO.receive (fun x -> FIO.Unit)))
+
 [<EntryPoint>]
 let main argv =
-
-    let p: FIO.Effect<string> =
-        FIO.receive (fun x -> FIO.receive (fun y -> FIO.send (x + y)))
-
-    (*
-    let addr = "127.0.0.1"
-    let port = 8000
-    let msg = "test"
-
-    async {
-        let! result = FIO.receiveFromSocket "127.0.0.1" 8000
-        printfn "Received: %s" result                               
-    } |> Async.Start
-
-    printfn "Sending message: %s" msg
-    FIO.sendToSocket "127.0.0.1" 8000 msg
-    *)
-
+    
     FIO.naiveEval p
-   
+    FIO.naiveEval p1
+    FIO.naiveEval p2
+
     0
