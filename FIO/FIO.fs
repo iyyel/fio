cadc6056fefb586432d26496a7700d3f4a71f404
iyyel <mail@iyyel.io>
2022-01-15 13:24:47 +0100

    Variable naming changes.

--- FIO/FIO.fs
+++ FIO/FIO.fs
@@ -5,32 +5,32 @@ open System.Collections.Concurrent
 module FIO =
 
     type Channel<'a>() =
-        let q = ConcurrentQueue<'a>()
+        let queue = ConcurrentQueue<'a>()
+    
+        member internal this.Send value =
+                queue.Enqueue value
+    
+        member internal this.Receive =
+                let status, value = queue.TryDequeue()
+                if status then value else this.Receive
         
-        member internal this.send v =
-                q.Enqueue v
-        
-        member internal this.receive() =
-                let s, v = q.TryDequeue()
-                if s then v else this.receive()
-            
     type Effect<'a> =
         | Input of Channel<'a> * ('a -> Effect<'a>)
         | Output of 'a * Channel<'a> * (unit -> Effect<'a>)
         | Parallel of Effect<'a> * Effect<'a>
         | Return of 'a
 
-    let send (v, c, f) = Output(v, c, f)
-    let receive (c, f) = Input(c, f)
+    let Send(value, chan, cont) = Output(value, chan, cont)
+    let Receive(chan, cont) = Input(chan, cont)
     
-    let rec naiveEval (e : Effect<'a>) : 'a =
-        match e with 
-        | Input(c, f)      -> let v = c.receive()
-                              naiveEval(f v)
-        | Output(v, c, f)  -> c.send v
-                              naiveEval(f ())
-        | Parallel(e1, e2) -> async {
-                                naiveEval e1 |> ignore // TODO: We should not ignore the result of the 'e1' effect.
-                              } |> Async.Start
-                              naiveEval e2
-        | Return v         -> v
+    let rec NaiveEval (eff : Effect<'a>) =
+        match eff with 
+        | Input(chan, cont)         -> let value = chan.Receive
+                                       NaiveEval(cont value)
+        | Output(value, chan, cont) -> chan.Send value
+                                       NaiveEval(cont ())
+        | Parallel(eff1, eff2)      -> async {
+                                           NaiveEval eff1 |> ignore
+                                       } |> Async.Start
+                                       NaiveEval eff2
+        | Return value              -> value
--- examples/Examples.fs
+++ examples/Examples.fs
@@ -4,33 +4,33 @@ let c = FIO.Channel<int>()
 
 let p1 c =
     let x = 0
-    FIO.send(x, c, fun () ->
+    FIO.Send(x, c, fun () ->
         printfn "p1 sent: %i" x
-        FIO.receive(c, fun y -> 
+        FIO.Receive(c, fun y -> 
             printfn "p1 received: %i" y
-            FIO.send(y, c, fun () ->
+            FIO.Send(y, c, fun () ->
                 printfn "p1 sent: %i" y
-                FIO.receive(c, fun z -> 
+                FIO.Receive(c, fun z -> 
                     printfn "p1 received: %i" z
                     FIO.Return z))))
 
 let p2 c =
-    FIO.receive(c, fun x -> 
+    FIO.Receive(c, fun x -> 
         printfn "p2 received: %i" x
         let y = x + 10
-        FIO.send(y, c, fun () -> 
+        FIO.Send(y, c, fun () -> 
             printfn "p2 sent: %i" y
-            FIO.receive(c, fun z -> 
+            FIO.Receive(c, fun z -> 
                 printfn "p2 received: %i" z
                 let v = z + 10
-                FIO.send(v, c, fun () -> 
+                FIO.Send(v, c, fun () -> 
                     printfn "p2 sent: %i" v
                     FIO.Return v))))
 
 [<EntryPoint>]
 let main argv =
     
-    let result = FIO.naiveEval(FIO.Parallel(p1 c, p2 c))
+    let result = FIO.NaiveEval(FIO.Parallel(p1 c, p2 c))
     printfn "Result: %i" result
 
     0
