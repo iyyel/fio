0d03f8ffd40c5dced9aa05b0c295230d9e05ddcd
iyyel <mail@iyyel.io>
2022-01-20 23:28:11 +0100

    hmm.

--- FIO/FIO.fs
+++ FIO/FIO.fs
@@ -26,11 +26,11 @@ module FIO =
         member internal this.Value = value
         member internal this.Chan = chan
         member internal this.Cont = cont
-    and Concurrent<'Result, 'Future>(eff: Effect<'Future>, cont: Async<'Future> -> Effect<'Result>) = 
+    and Concurrent<'Result, 'Async>(eff: Effect<'Async>, cont: Async<'Async> -> Effect<'Result>) = 
         inherit Effect<'Result>()
         member internal this.Eff = eff
         member internal this.Cont = cont
-    and Await<'Result, 'Future>(future: Async<'Future>, cont: 'Future -> Effect<'Result>) =
+    and Await<'Result, 'Async>(future: Async<'Async>, cont: 'Async -> Effect<'Result>) =
         inherit Effect<'Result>()
         member internal this.Future = future
         member internal this.Cont = cont
@@ -41,18 +41,18 @@ module FIO =
     let Send(value, chan, cont) = Output(value, chan, cont)
     let Receive(chan, cont) = Input(chan, cont)
     
-    let rec NaiveEval (eff : Effect<_>) : _ =
+    let rec NaiveEval (eff : Effect<'Result>) : 'Result =
         match eff with
         | :? Input<'Result> as input             -> let value = input.Chan.Receive
                                                     NaiveEval <| input.Cont value
         | :? Output<'Result> as output           -> output.Chan.Send output.Value
                                                     NaiveEval <| output.Cont ()
-        | :? Concurrent<'Result, 'Future> as con -> let work = async {
+        | :? Concurrent<'Result, 'Async> as con  -> let work = async {
                                                         return NaiveEval con.Eff
                                                     }
                                                     let task = Async.AwaitTask <| Async.StartAsTask work
                                                     NaiveEval <| con.Cont task
-        | :? Await<'Result, 'Future> as await    -> let res = Async.RunSynchronously await.Future
+        | :? Await<'Result, 'Async> as await     -> let res = Async.RunSynchronously await.Future
                                                     NaiveEval <| await.Cont res
         | :? Return<'Result> as ret              -> ret.Value
         | _                                      -> failwith "Unsupported effect!"
