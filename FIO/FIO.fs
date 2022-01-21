8ef797f919045a4583130f9c25598dba061938e8
iyyel <mail@iyyel.io>
2022-01-21 19:01:26 +0100

    Working visitor implementation.

--- FIO/FIO.fs
+++ FIO/FIO.fs
@@ -4,8 +4,8 @@ open System.Collections.Concurrent
 
 module FIO =
 
-    type Channel<'a>() =
-        let queue = ConcurrentQueue<'a>()
+    type Channel<'Result>() =
+        let queue = ConcurrentQueue<'Result>()
     
         member internal this.Send value =
                 queue.Enqueue value
@@ -14,45 +14,68 @@ module FIO =
                 let status, value = queue.TryDequeue()
                 if status then value else this.Receive
 
-    [<AbstractClass>]
-    type Effect<'Result>() =
-        class end
-    and Input<'Result>(chan : Channel<'Result>, cont : 'Result -> Effect<'Result>) = 
-        inherit Effect<'Result>()
-        member internal this.Chan = chan
-        member internal this.Cont = cont
+    type EffectVisitor =
+      abstract member VisitInput<'Result> : Input<'Result> -> 'Result
+      abstract member VisitOutput<'Result> : Output<'Result> -> 'Result
+      abstract member VisitConcurrent<'Result, 'Async> : Concurrent<'Result, 'Async> -> 'Result
+      abstract member VisitAwait<'Result, 'Async> : Await<'Result, 'Async> -> 'Result
+      abstract member VisitReturn<'Result> : Return<'Result> -> 'Result
+    and [<AbstractClass>] Effect() =
+        abstract member Visit : EffectVisitor -> 'Result
+    and [<AbstractClass>] Effect<'Result>() =
+        abstract member Visit<'Result> : EffectVisitor -> 'Result
+    and Input<'Result>(chan : Channel<'Result>, cont : 'Result -> Effect<'Result>) =
+      inherit Effect<'Result>()
+      member internal this.Chan = chan
+      member internal this.Cont = cont
+      override this.Visit<'Result>(input) =
+        input.VisitInput<'Result>(this)
     and Output<'Result>(value : 'Result, chan : Channel<'Result>, cont : unit -> Effect<'Result>) =
-        inherit Effect<'Result>()
-        member internal this.Value = value
-        member internal this.Chan = chan
-        member internal this.Cont = cont
-    and Concurrent<'Result, 'Async>(eff: Effect<'Async>, cont: Async<'Async> -> Effect<'Result>) = 
+      inherit Effect<'Result>()
+      member internal this.Value = value
+      member internal this.Chan = chan
+      member internal this.Cont = cont
+      override this.Visit<'Result>(input) =
+        input.VisitOutput<'Result>(this)
+    and Concurrent<'Result, 'Async>(eff : Effect<'Async>, cont: Async<'Async> -> Effect<'Result>) =
         inherit Effect<'Result>()
         member internal this.Eff = eff
         member internal this.Cont = cont
-    and Await<'Result, 'Async>(future: Async<'Async>, cont: 'Async -> Effect<'Result>) =
+        override this.Visit<'Result>(con) =
+            con.VisitConcurrent<'Result, 'Async>(this)
+    and Await<'Result, 'Async>(task: Async<'Async>, cont: 'Async -> Effect<'Result>) =
         inherit Effect<'Result>()
-        member internal this.Future = future
+        member internal this.Task = task
         member internal this.Cont = cont
+        override this.Visit<'Result>(await) =
+            await.VisitAwait<'Result, 'Async>(this)
     and Return<'Result>(value : 'Result) =
         inherit Effect<'Result>()
         member internal this.Value = value
+        override this.Visit<'Result>(input) =
+            input.VisitReturn<'Result>(this)
 
     let Send(value, chan, cont) = Output(value, chan, cont)
     let Receive(chan, cont) = Input(chan, cont)
-    
-    let rec NaiveEval (eff : Effect<'Result>) : 'Result =
-        match eff with
-        | :? Input<'Result> as input             -> let value = input.Chan.Receive
-                                                    NaiveEval <| input.Cont value
-        | :? Output<'Result> as output           -> output.Chan.Send output.Value
-                                                    NaiveEval <| output.Cont ()
-        | :? Concurrent<'Result, 'Async> as con  -> let work = async {
-                                                        return NaiveEval con.Eff
-                                                    }
-                                                    let task = Async.AwaitTask <| Async.StartAsTask work
-                                                    NaiveEval <| con.Cont task
-        | :? Await<'Result, 'Async> as await     -> let res = Async.RunSynchronously await.Future
-                                                    NaiveEval <| await.Cont res
-        | :? Return<'Result> as ret              -> ret.Value
-        | _                                      -> failwith "Unsupported effect!"
+
+    let rec NaiveEval<'Result> (eff : Effect<'Result>) : 'Result =
+        eff.Visit({
+            new EffectVisitor with
+                member this.VisitInput<'Result>(input : Input<'Result>) : 'Result =
+                    let value = input.Chan.Receive
+                    NaiveEval <| input.Cont value
+                member this.VisitOutput<'Result>(output : Output<'Result>) : 'Result =
+                    output.Chan.Send output.Value
+                    NaiveEval <| output.Cont ()
+                member this.VisitConcurrent(con) =
+                    let work = async {
+                        return NaiveEval con.Eff
+                    }
+                    let task = Async.AwaitTask <| Async.StartAsTask work
+                    NaiveEval <| con.Cont task
+                member this.VisitAwait(await) =
+                    let result = Async.RunSynchronously await.Task
+                    NaiveEval <| await.Cont result
+                member this.VisitReturn<'Result>(ret : Return<'Result>) : 'Result =
+                    ret.Value
+        })
--- examples/Examples.fs
+++ examples/Examples.fs
@@ -1,30 +1,56 @@
 ﻿open FSharp.FIO
 
 let chanInt = FIO.Channel<int>()
+let chanStr = FIO.Channel<string>()
 
-let p1 chan =
+let p1 c =
     let x = 0
-    FIO.Send(x, chan, fun () ->
-        printfn $"p1 sent: %i{x}"
-        FIO.Receive(chan, fun y -> 
-            printfn $"p1 received: %i{y}"
-            FIO.Send(y, chan, fun () ->
-                printfn $"p1 sent: %i{y}"
-                FIO.Receive(chan, fun z -> 
-                    printfn $"p1 received: %i{z}"
+    FIO.Send(x, c, fun () ->
+        printfn "p1 sent: %i" x
+        FIO.Receive(c, fun y ->
+            printfn "p1 received: %i" y
+            FIO.Send(y, c, fun () ->
+                printfn "p1 sent: %i" y
+                FIO.Receive(c, fun z -> 
+                    printfn "p1 received: %i" z
                     FIO.Return z))))
 
-let p2 chan =
-    FIO.Receive(chan, fun x -> 
-        printfn $"p2 received: %i{x}"
+let p2 c =
+    FIO.Receive(c, fun x ->
+        printfn "p2 received: %i" x
         let y = x + 10
-        FIO.Send(y, chan, fun () -> 
-            printfn $"p2 sent: %i{y}"
-            FIO.Receive(chan, fun z -> 
-                printfn $"p2 received: %i{z}"
+        FIO.Send(y, c, fun () ->
+            printfn "p2 sent: %i" y
+            FIO.Receive(c, fun z ->
+                printfn "p2 received: %i" z
                 let v = z + 10
-                FIO.Send(v, chan, fun () -> 
-                    printfn $"p2 sent: %i{v}"
+                FIO.Send(v, c, fun () ->
+                    printfn "p2 sent: %i" v
+                    FIO.Return v))))
+
+let p11 c =
+    let x = ""
+    FIO.Send(x, c, fun () ->
+        printfn "p1 sent: %s" x
+        FIO.Receive(c, fun y ->
+            printfn "p1 received: %s" y
+            FIO.Send(y, c, fun () ->
+                printfn "p1 sent: %s" y
+                FIO.Receive(c, fun z ->
+                    printfn "p1 received: %s" z
+                    FIO.Return z))))
+
+let p22 c =
+    FIO.Receive(c, fun x ->
+        printfn "p2 received: %s" x
+        let y = x + "a"
+        FIO.Send(y, c, fun () ->
+            printfn "p2 sent: %s" y
+            FIO.Receive(c, fun z ->
+                printfn "p2 received: %s" z
+                let v = z + "b"
+                FIO.Send(v, c, fun () ->
+                    printfn "p2 sent: %s" v
                     FIO.Return v))))
 
 let p3 chan =
@@ -34,10 +60,26 @@ let p3 chan =
                 FIO.Await(t2, fun res2 ->
                     FIO.Return(res1 + res2)))))
 
-[<EntryPoint>]
-let main argv =
+let p33 chan =
+    FIO.Concurrent(p11 chan, fun t1 ->
+        FIO.Concurrent(p22 chan, fun t2 ->
+            FIO.Await(t1, fun res1 ->
+                FIO.Await(t2, fun res2 ->
+                    FIO.Return(res1 + res2)))))
 
-    let result = FIO.NaiveEval(p3 chanInt)
-    printfn $"Result: %A{result}"
-    
+let p10 chanInt chanStr =
+    FIO.Concurrent(p3 chanInt, fun t1 ->
+        FIO.Concurrent(p33 chanStr, fun t2 ->
+            FIO.Await(t1, fun res1 ->
+                FIO.Await(t2, fun res2 ->
+                    printfn "int result: %i" res1
+                    printfn "string result: %s" res2
+                    FIO.Return(res1)))))
+
+[<EntryPoint>]
+let main _ =
+
+    let result = FIO.NaiveEval(p10 chanInt chanStr)
+    printfn "Result: %i" result
+
     0
