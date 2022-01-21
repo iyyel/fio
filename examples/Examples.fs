d22ef3ca62e34c9d3544d44ae8c5f4a41a971518
iyyel <mail@iyyel.io>
2022-01-21 19:05:08 +0100

    Code formatting.

--- examples/Examples.fs
+++ examples/Examples.fs
@@ -6,51 +6,51 @@ let chanStr = FIO.Channel<string>()
 let p1 c =
     let x = 0
     FIO.Send(x, c, fun () ->
-        printfn "p1 sent: %i" x
+        printfn $"p1 sent: %i{x}"
         FIO.Receive(c, fun y ->
-            printfn "p1 received: %i" y
+            printfn $"p1 received: %i{y}"
             FIO.Send(y, c, fun () ->
-                printfn "p1 sent: %i" y
+                printfn $"p1 sent: %i{y}"
                 FIO.Receive(c, fun z -> 
-                    printfn "p1 received: %i" z
+                    printfn $"p1 received: %i{z}"
                     FIO.Return z))))
 
 let p2 c =
     FIO.Receive(c, fun x ->
-        printfn "p2 received: %i" x
+        printfn $"p2 received: %i{x}"
         let y = x + 10
         FIO.Send(y, c, fun () ->
-            printfn "p2 sent: %i" y
+            printfn $"p2 sent: %i{y}"
             FIO.Receive(c, fun z ->
-                printfn "p2 received: %i" z
+                printfn $"p2 received: %i{z}"
                 let v = z + 10
                 FIO.Send(v, c, fun () ->
-                    printfn "p2 sent: %i" v
+                    printfn $"p2 sent: %i{v}"
                     FIO.Return v))))
 
 let p11 c =
     let x = ""
     FIO.Send(x, c, fun () ->
-        printfn "p1 sent: %s" x
+        printfn $"p1 sent: %s{x}"
         FIO.Receive(c, fun y ->
-            printfn "p1 received: %s" y
+            printfn $"p1 received: %s{y}"
             FIO.Send(y, c, fun () ->
-                printfn "p1 sent: %s" y
+                printfn $"p1 sent: %s{y}"
                 FIO.Receive(c, fun z ->
-                    printfn "p1 received: %s" z
+                    printfn $"p1 received: %s{z}"
                     FIO.Return z))))
 
 let p22 c =
     FIO.Receive(c, fun x ->
-        printfn "p2 received: %s" x
+        printfn $"p2 received: %s{x}"
         let y = x + "a"
         FIO.Send(y, c, fun () ->
-            printfn "p2 sent: %s" y
+            printfn $"p2 sent: %s{y}"
             FIO.Receive(c, fun z ->
-                printfn "p2 received: %s" z
+                printfn $"p2 received: %s{z}"
                 let v = z + "b"
                 FIO.Send(v, c, fun () ->
-                    printfn "p2 sent: %s" v
+                    printfn $"p2 sent: %s{v}"
                     FIO.Return v))))
 
 let p3 chan =
@@ -72,14 +72,14 @@ let p10 chanInt chanStr =
         FIO.Concurrent(p33 chanStr, fun t2 ->
             FIO.Await(t1, fun res1 ->
                 FIO.Await(t2, fun res2 ->
-                    printfn "int result: %i" res1
-                    printfn "string result: %s" res2
+                    printfn $"int result: %i{res1}"
+                    printfn $"string result: %s{res2}"
                     FIO.Return(res1)))))
 
 [<EntryPoint>]
 let main _ =
 
     let result = FIO.NaiveEval(p10 chanInt chanStr)
-    printfn "Result: %i" result
+    printfn $"Result: %i{result}"
 
     0
