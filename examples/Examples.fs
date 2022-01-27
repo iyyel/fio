7788a91c7f101054edfb0a7e6b60c99dc5cb14b4
iyyel <mail@iyyel.io>
2022-01-27 22:35:30 +0100

    Test

--- examples/Examples.fs
+++ examples/Examples.fs
@@ -8,6 +8,7 @@ let main _ =
     let chanInt = FIO.Channel<int>()
     let chanStr = FIO.Channel<string>()
 
+
     let result = FIO.NaiveEval(intStrPingpongInf chanInt chanStr)
     printfn $"Result: %A{result}"
     
