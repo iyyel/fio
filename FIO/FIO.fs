ea8b14b2ea8d486154f7d733e9d99f43a0033dfb
iyyel <mail@iyyel.io>
2022-01-27 22:01:56 +0100

    Refactoring + ring benchmark progress.

--- FIO/FIO.fs
+++ FIO/FIO.fs
@@ -6,10 +6,10 @@ module FIO =
 
     type Channel<'Result>() =
         let queue = ConcurrentQueue<'Result>()
-    
+
         member internal this.Send value =
             queue.Enqueue value
-    
+
         member internal this.Receive =
             let status, value = queue.TryDequeue()
             if status then value else this.Receive
@@ -19,10 +19,7 @@ module FIO =
         abstract member VisitOutput<'Result> : Output<'Result> -> 'Result
         abstract member VisitConcurrent<'Result, 'Async> : Concurrent<'Result, 'Async> -> 'Result
         abstract member VisitAwait<'Result, 'Async> : Await<'Result, 'Async> -> 'Result
-        abstract member VisitParallel<'ResultA, 'ResultB> : Parallel<'ResultA, 'ResultB> -> 'ResultA * 'ResultB
         abstract member VisitReturn<'Result> : Return<'Result> -> 'Result
-    and [<AbstractClass>] Effect() =
-        abstract member Visit : EffectVisitor -> 'Result
     and [<AbstractClass>] Effect<'Result>() =
         abstract member Visit<'Result> : EffectVisitor -> 'Result
     and Input<'Result>(chan : Channel<'Result>, cont : 'Result -> Effect<'Result>) =
@@ -50,12 +47,6 @@ module FIO =
         member internal this.Cont = cont
         override this.Visit<'Result>(await) =
             await.VisitAwait<'Result, 'Async>(this)
-    and Parallel<'ResultA, 'ResultB>(effA : Effect<'ResultA>, effB : Effect<'ResultB>) =
-        inherit Effect<'ResultA * 'ResultB>()
-        member internal this.EffA = effA
-        member internal this.EffB = effB
-        override this.Visit<'ResultA>(par) =
-            par.VisitParallel<'ResultA, 'ResultB>(this)
     and Return<'Result>(value : 'Result) =
         inherit Effect<'Result>()
         member internal this.Value = value
@@ -64,32 +55,30 @@ module FIO =
 
     let Send(value, chan, cont) = Output(value, chan, cont)
     let Receive(chan, cont) = Input(chan, cont)
+    let Parallel(effA, effB) = Concurrent(effA, fun asyncA ->
+                                   Concurrent(effB, fun asyncB ->
+                                       Await(asyncA, fun resultA ->
+                                           Await(asyncB, fun resultB ->
+                                               Return((resultA, resultB))))))
 
-    let rec NaiveEval<'Result> (eff : Effect<'Result>) : 'Result =
-        eff.Visit({
-            new EffectVisitor with
-                member _.VisitInput<'Result>(input : Input<'Result>) : 'Result =
-                    let value = input.Chan.Receive
-                    NaiveEval <| input.Cont value
-                member _.VisitOutput<'Result>(output : Output<'Result>) : 'Result =
-                    output.Chan.Send output.Value
-                    NaiveEval <| output.Cont ()
-                member _.VisitConcurrent(con) =
-                    let work = async {
-                        return NaiveEval con.Eff
-                    }
-                    let task = Async.AwaitTask <| Async.StartAsTask work
-                    NaiveEval <| con.Cont task
-                member _.VisitAwait(await) =
-                    let result = Async.RunSynchronously await.Task
-                    NaiveEval <| await.Cont result
-                member _.VisitParallel(par) =
-                    let p = Concurrent(par.EffA, fun taskA ->
-                                Concurrent(par.EffB, fun taskB ->
-                                    Await(taskA, fun resultA ->
-                                        Await(taskB, fun resultB ->
-                                            Return((resultA, resultB))))))
-                    NaiveEval p
-                member _.VisitReturn<'Result>(ret : Return<'Result>) : 'Result =
-                    ret.Value
-        })
+    let rec NaiveEval<'Result> (eff : Effect<'Result>) =
+        eff.Visit(effectVisitor)
+    and effectVisitor = { new EffectVisitor with
+                            member _.VisitInput<'Result>(input : Input<'Result>) =
+                                let value = input.Chan.Receive
+                                NaiveEval <| input.Cont value
+                            member _.VisitOutput<'Result>(output : Output<'Result>) =
+                                output.Chan.Send output.Value
+                                NaiveEval <| output.Cont ()
+                            member _.VisitConcurrent(con) =
+                                let work = async {
+                                    return NaiveEval con.Eff
+                                }
+                                let async' = Async.AwaitTask <| Async.StartAsTask work
+                                NaiveEval <| con.Cont async'
+                            member _.VisitAwait(await) =
+                                let result = Async.RunSynchronously await.Task
+                                NaiveEval <| await.Cont result
+                            member _.VisitReturn<'Result>(ret : Return<'Result>) =
+                                ret.Value
+                       }
--- examples/Examples.fs
+++ examples/Examples.fs
@@ -1,134 +1,189 @@
 ﻿open FSharp.FIO
 
-
 module Pingpong =
 
     let intPing chanInt =
         let x = 10
         FIO.Send(x, chanInt, fun () ->
-            printfn $"intPing sent: %i{x}"
+            printfn $"intPing sent: %A{x}"
             FIO.Receive(chanInt, fun y ->
-                printfn $"intPing received: %i{y}"
+                printfn $"intPing received: %A{y}"
                 FIO.Send(y, chanInt, fun () ->
-                    printfn $"intPing sent: %i{y}"
+                    printfn $"intPing sent: %A{y}"
                     FIO.Receive(chanInt, fun z ->
-                        printfn $"intPing received: %i{z}"
+                        printfn $"intPing received: %A{z}"
                         FIO.Return z))))
 
     let intPong chanInt =
         FIO.Receive(chanInt, fun x ->
-            printfn $"intPong received: %i{x}"
+            printfn $"intPong received: %A{x}"
             let y = x + 10
             FIO.Send(y, chanInt, fun () ->
-                printfn $"intPong sent: %i{y}"
+                printfn $"intPong sent: %A{y}"
                 FIO.Receive(chanInt, fun z ->
-                    printfn $"intPong received: %i{z}"
+                    printfn $"intPong received: %A{z}"
                     let v = z + 10
                     FIO.Send(v, chanInt, fun () ->
-                        printfn $"intPong sent: %i{v}"
+                        printfn $"intPong sent: %A{v}"
                         FIO.Return v))))
+
+    let rec intPingInf chanInt =
+        let x = 10
+        FIO.Send(x, chanInt, fun () ->
+            printfn $"intPing sent: %A{x}"
+            FIO.Receive(chanInt, fun y ->
+                printfn $"intPing received: %A{y}"
+                intPingInf chanInt))
+
+    let rec intPongInf chanInt =
+        FIO.Receive(chanInt, fun x ->
+            printfn $"intPong received: %A{x}"
+            let y = x + 10
+            FIO.Send(y, chanInt, fun () ->
+                printfn $"intPong sent: %A{y}"
+                intPongInf chanInt))
 
     let strPing chanStr =
         let x = ""
         FIO.Send(x, chanStr, fun () ->
-            printfn $"strPing sent: %s{x}"
+            printfn $"strPing sent: %A{x}"
             FIO.Receive(chanStr, fun y ->
-                printfn $"strPing received: %s{y}"
+                printfn $"strPing received: %A{y}"
                 FIO.Send(y, chanStr, fun () ->
-                    printfn $"strPing sent: %s{y}"
+                    printfn $"strPing sent: %A{y}"
                     FIO.Receive(chanStr, fun z ->
-                        printfn $"strPing received: %s{z}"
+                        printfn $"strPing received: %A{z}"
                         FIO.Return z))))
 
     let strPong chanStr =
         FIO.Receive(chanStr, fun x ->
-            printfn $"strPong received: %s{x}"
+            printfn $"strPong received: %A{x}"
             let y = x + "a"
             FIO.Send(y, chanStr, fun () ->
-                printfn $"strPong sent: %s{y}"
+                printfn $"strPong sent: %A{y}"
                 FIO.Receive(chanStr, fun z ->
-                    printfn $"strPong received: %s{z}"
+                    printfn $"strPong received: %A{z}"
                     let v = z + "b"
                     FIO.Send(v, chanStr, fun () ->
-                        printfn $"strPong sent: %s{v}"
+                        printfn $"strPong sent: %A{v}"
                         FIO.Return v))))
+
+    let rec strPingInf chanStr =
+        let x = ""
+        FIO.Send(x, chanStr, fun () ->
+            printfn $"strPing sent: %A{x}"
+            FIO.Receive(chanStr, fun y ->
+                printfn $"strPing received: %A{y}"
+                strPingInf chanStr))
+
+    let rec strPongInf chanStr =
+        FIO.Receive(chanStr, fun x ->
+            printfn $"strPong received: %A{x}"
+            let y = x + "a"
+            FIO.Send(y, chanStr, fun () ->
+                printfn $"strPong sent: %A{y}"
+                strPongInf chanStr))
 
     let intPingpong chanInt =
         FIO.Parallel(intPing chanInt, intPong chanInt)
 
+    let intPingpongInf chanInt =
+        FIO.Parallel(intPingInf chanInt, intPongInf chanInt)
+
     let strPingpong chanStr =
         FIO.Parallel(strPing chanStr, strPong chanStr)
+
+    let strPingpongInf chanStr =
+        FIO.Parallel(strPingInf chanStr, strPongInf chanStr)
 
     let intStrPingpong chanInt chanStr =
         FIO.Parallel(intPingpong chanInt, strPingpong chanStr)
 
+    let intStrPingpongInf chanInt chanStr =
+        FIO.Parallel(intPingpongInf chanInt, strPingpongInf chanStr)
 
 module Ring =
 
-    let rec repeat n template eff =
+    let rec internal repeat n template eff =
         if n = 1 then
             eff
         else 
             repeat (n - 1) template (template eff)
 
-    let channelPair<'a> =
-        let chan1 = FIO.Channel<'a>()
-        let chan2 = FIO.Channel<'a>()
-        (chan1, chan2)
-
-    let spawnSendProcess chanSend chanRecv value name m =
-        let eff = 
+    let internal spawnSendProcess chanSend chanRecv value name m =
+        let eff =
             FIO.Send(value, chanSend, fun () ->
-                printfn $"%s{name} sent: %i{value}"
+                printfn $"%s{name} sent: %A{value}"
                 FIO.Receive(chanRecv, fun v ->
-                    printfn $"%s{name} received: %i{v}"
+                    printfn $"%s{name} received: %A{v}"
                     FIO.Return v))
         let template eff = 
             FIO.Send(value, chanSend, fun () ->
-                printfn $"%s{name} sent: %i{value}"
+                printfn $"%s{name} sent: %A{value}"
                 FIO.Receive(chanRecv, fun v ->
-                    printfn $"%s{name} received: %i{v}"
+                    printfn $"%s{name} received: %A{v}"
                     eff))
         repeat m template eff
 
-    let spawnRecvProcess chanRecv chanSend name m =
+    let internal spawnRecvProcess chanRecv chanSend name m =
         let eff = 
             FIO.Receive(chanRecv, fun v ->
-                printfn $"%s{name} received: %i{v}"
+                printfn $"%s{name} received: %A{v}"
                 let value = v + 10
                 FIO.Send(value, chanSend, fun () ->
-                    printfn $"%s{name} sent: %i{value}"
+                    printfn $"%s{name} sent: %A{value}"
                     FIO.Return value))
         let template eff = 
             FIO.Receive(chanRecv, fun v ->
-                printfn $"%s{name} received: %i{v}"
+                printfn $"%s{name} received: %A{v}"
                 let value = v + 10
                 FIO.Send(value, chanSend, fun () ->
-                    printfn $"%s{name} sent: %i{value}"
+                    printfn $"%s{name} sent: %A{value}"
                     eff))
         repeat m template eff
 
-    let ring n m = 
-        let chan1 = FIO.Channel<int>()
-        let chan2 = FIO.Channel<int>()
-        let chan3 = FIO.Channel<int>()
-        let chan4 = FIO.Channel<int>()
-        let chan5 = FIO.Channel<int>()
+    type internal Process =
+        { Name: string
+          ChanSend: FIO.Channel<int>
+          ChanRecv: FIO.Channel<int>
+        }
 
-        FIO.Parallel(spawnSendProcess chan1 chan5 0 "p1" m, 
-            FIO.Parallel(spawnRecvProcess chan1 chan2 "p2" m,
-                FIO.Parallel(spawnRecvProcess chan2 chan3 "p3" m, 
-                    FIO.Parallel(spawnRecvProcess chan3 chan4 "p4" m, 
-                        spawnRecvProcess chan4 chan5 "p5" m))))
+    let internal getRecvChannel index (chans : FIO.Channel<int> list) =
+        match index with
+        | i when i - 1 < 0 -> chans.Item (List.length chans - 1)
+        | i                -> chans.Item (i - 1)
 
+    let ring n m =
+        let chans = [for _ in 1..n -> FIO.Channel<int>()]
+        let mutable index = 0;
+        let mutable processes = []
 
+        for chan in chans do
+            let proc = {Name = $"p{index}"; ChanSend = chan; ChanRecv = getRecvChannel index chans}
+            processes <- List.append [proc] processes // in reverse order
+            index <- index + 1
+
+        let rec repeat n template eff chans m =
+            if n = 1 then
+                eff
+            else 
+                repeat (n - 1) template (template (n - 1) eff chans m) chans m
+  
+        let eff = let (px, py) = (List.item 0 processes, List.item 1 processes)
+                  FIO.Concurrent(spawnRecvProcess py.ChanRecv py.ChanSend py.Name m, fun asyncPy -> 
+                      FIO.Concurrent(spawnRecvProcess px.ChanRecv px.ChanSend px.Name m, fun asyncPx ->
+                          FIO.Await(asyncPy, fun _ -> 
+                              FIO.Await(asyncPx, fun res -> FIO.Return res))))
+
+        ()
+        
 [<EntryPoint>]
 let main _ =
 
     let chanInt = FIO.Channel<int>()
     let chanStr = FIO.Channel<string>()
 
-    let result = FIO.NaiveEval(Ring.ring 0 1)
+    let result = FIO.NaiveEval(Pingpong.intStrPingpongInf chanInt chanStr)
     printfn $"Result: %A{result}"
 
     0
