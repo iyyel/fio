81d54589e4ffb0b89c47250be968cb2ed5aba11f
iyyel <mail@iyyel.io>
2022-01-28 22:13:13 +0100

    Add files again.

---
+++ Examples/Program.fs
@@ -0,0 +1,15 @@
+﻿module Program
+
+open FSharp.FIO
+open Examples
+
+[<EntryPoint>]
+let main _ =
+
+    let chanInt = Channel<int>()
+    let chanStr = Channel<string>()
+
+    let result = NaiveEval(Ring.processRing 10 1000)
+    printfn $"Result: %A{result}"
+
+    0
