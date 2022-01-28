8ec1146f0387985deb6c07f80e5486580a4ca65a
iyyel <mail@iyyel.io>
2022-01-28 22:02:11 +0100

    Project restructure.

--- FIO/FIO.fs
+++ FIO/FIO.fs
@@ -1,84 +1,82 @@
-﻿namespace FSharp.FIO
+﻿module FSharp.FIO
 
 open System.Collections.Concurrent
 
-module FIO =
+type Channel<'Result>() =
+    let queue = ConcurrentQueue<'Result>()
 
-    type Channel<'Result>() =
-        let queue = ConcurrentQueue<'Result>()
+    member internal this.Send value =
+        queue.Enqueue value
 
-        member internal this.Send value =
-            queue.Enqueue value
+    member internal this.Receive =
+        let status, value = queue.TryDequeue()
+        if status then value else this.Receive
 
-        member internal this.Receive =
-            let status, value = queue.TryDequeue()
-            if status then value else this.Receive
+type EffectVisitor =
+    abstract member VisitInput<'Result> : Input<'Result> -> 'Result
+    abstract member VisitOutput<'Result> : Output<'Result> -> 'Result
+    abstract member VisitConcurrent<'Result, 'Async> : Concurrent<'Result, 'Async> -> 'Result
+    abstract member VisitAwait<'Result, 'Async> : Await<'Result, 'Async> -> 'Result
+    abstract member VisitReturn<'Result> : Return<'Result> -> 'Result
+and [<AbstractClass>] Effect<'Result>() =
+    abstract member Visit<'Result> : EffectVisitor -> 'Result
+and Input<'Result>(chan : Channel<'Result>, cont : 'Result -> Effect<'Result>) =
+    inherit Effect<'Result>()
+    member internal this.Chan = chan
+    member internal this.Cont = cont
+    override this.Visit<'Result>(input) =
+        input.VisitInput<'Result>(this)
+and Output<'Result>(value : 'Result, chan : Channel<'Result>, cont : unit -> Effect<'Result>) =
+    inherit Effect<'Result>()
+    member internal this.Value = value
+    member internal this.Chan = chan
+    member internal this.Cont = cont
+    override this.Visit<'Result>(input) =
+        input.VisitOutput<'Result>(this)
+and Concurrent<'Result, 'Async>(eff : Effect<'Async>, cont : Async<'Async> -> Effect<'Result>) =
+    inherit Effect<'Result>()
+    member internal this.Eff = eff
+    member internal this.Cont = cont
+    override this.Visit<'Result>(con) =
+        con.VisitConcurrent<'Result, 'Async>(this)
+and Await<'Result, 'Async>(task : Async<'Async>, cont : 'Async -> Effect<'Result>) =
+    inherit Effect<'Result>()
+    member internal this.Task = task
+    member internal this.Cont = cont
+    override this.Visit<'Result>(await) =
+        await.VisitAwait<'Result, 'Async>(this)
+and Return<'Result>(value : 'Result) =
+    inherit Effect<'Result>()
+    member internal this.Value = value
+    override this.Visit<'Result>(input) =
+        input.VisitReturn<'Result>(this)
 
-    type EffectVisitor =
-        abstract member VisitInput<'Result> : Input<'Result> -> 'Result
-        abstract member VisitOutput<'Result> : Output<'Result> -> 'Result
-        abstract member VisitConcurrent<'Result, 'Async> : Concurrent<'Result, 'Async> -> 'Result
-        abstract member VisitAwait<'Result, 'Async> : Await<'Result, 'Async> -> 'Result
-        abstract member VisitReturn<'Result> : Return<'Result> -> 'Result
-    and [<AbstractClass>] Effect<'Result>() =
-        abstract member Visit<'Result> : EffectVisitor -> 'Result
-    and Input<'Result>(chan : Channel<'Result>, cont : 'Result -> Effect<'Result>) =
-        inherit Effect<'Result>()
-        member internal this.Chan = chan
-        member internal this.Cont = cont
-        override this.Visit<'Result>(input) =
-            input.VisitInput<'Result>(this)
-    and Output<'Result>(value : 'Result, chan : Channel<'Result>, cont : unit -> Effect<'Result>) =
-        inherit Effect<'Result>()
-        member internal this.Value = value
-        member internal this.Chan = chan
-        member internal this.Cont = cont
-        override this.Visit<'Result>(input) =
-            input.VisitOutput<'Result>(this)
-    and Concurrent<'Result, 'Async>(eff : Effect<'Async>, cont : Async<'Async> -> Effect<'Result>) =
-        inherit Effect<'Result>()
-        member internal this.Eff = eff
-        member internal this.Cont = cont
-        override this.Visit<'Result>(con) =
-            con.VisitConcurrent<'Result, 'Async>(this)
-    and Await<'Result, 'Async>(task : Async<'Async>, cont : 'Async -> Effect<'Result>) =
-        inherit Effect<'Result>()
-        member internal this.Task = task
-        member internal this.Cont = cont
-        override this.Visit<'Result>(await) =
-            await.VisitAwait<'Result, 'Async>(this)
-    and Return<'Result>(value : 'Result) =
-        inherit Effect<'Result>()
-        member internal this.Value = value
-        override this.Visit<'Result>(input) =
-            input.VisitReturn<'Result>(this)
+let Send(value, chan, cont) = Output(value, chan, cont)
+let Receive(chan, cont) = Input(chan, cont)
+let Parallel(effA, effB) = Concurrent(effA, fun asyncA ->
+                                Concurrent(effB, fun asyncB ->
+                                    Await(asyncA, fun resultA ->
+                                        Await(asyncB, fun resultB ->
+                                            Return((resultA, resultB))))))
 
-    let Send(value, chan, cont) = Output(value, chan, cont)
-    let Receive(chan, cont) = Input(chan, cont)
-    let Parallel(effA, effB) = Concurrent(effA, fun asyncA ->
-                                   Concurrent(effB, fun asyncB ->
-                                       Await(asyncA, fun resultA ->
-                                           Await(asyncB, fun resultB ->
-                                               Return((resultA, resultB))))))
-
-    let rec NaiveEval<'Result> (eff : Effect<'Result>) =
-        eff.Visit(effectVisitor)
-    and effectVisitor = { new EffectVisitor with
-                            member _.VisitInput<'Result>(input : Input<'Result>) =
-                                let value = input.Chan.Receive
-                                NaiveEval <| input.Cont value
-                            member _.VisitOutput<'Result>(output : Output<'Result>) =
-                                output.Chan.Send output.Value
-                                NaiveEval <| output.Cont ()
-                            member _.VisitConcurrent(con) =
-                                let work = async {
-                                    return NaiveEval con.Eff
-                                }
-                                let async' = Async.AwaitTask <| Async.StartAsTask work
-                                NaiveEval <| con.Cont async'
-                            member _.VisitAwait(await) =
-                                let result = Async.RunSynchronously await.Task
-                                NaiveEval <| await.Cont result
-                            member _.VisitReturn<'Result>(ret : Return<'Result>) =
-                                ret.Value
-                       }
+let rec NaiveEval<'Result> (eff : Effect<'Result>) =
+    eff.Visit(effectVisitor)
+and effectVisitor = { new EffectVisitor with
+                        member _.VisitInput<'Result>(input : Input<'Result>) =
+                            let value = input.Chan.Receive
+                            NaiveEval <| input.Cont value
+                        member _.VisitOutput<'Result>(output : Output<'Result>) =
+                            output.Chan.Send output.Value
+                            NaiveEval <| output.Cont ()
+                        member _.VisitConcurrent(con) =
+                            let work = async {
+                                return NaiveEval con.Eff
+                            }
+                            let async' = Async.AwaitTask <| Async.StartAsTask work
+                            NaiveEval <| con.Cont async'
+                        member _.VisitAwait(await) =
+                            let result = Async.RunSynchronously await.Task
+                            NaiveEval <| await.Cont result
+                        member _.VisitReturn<'Result>(ret : Return<'Result>) =
+                            ret.Value
+                    }
--- examples/Examples.fs
+++ examples/Examples.fs
@@ -1,14 +1,179 @@
-﻿open FSharp.FIO
-open Pingpong
-open Ring
- 
-[<EntryPoint>]
-let main _ =
+﻿namespace Examples
 
-    let chanInt = FIO.Channel<int>()
-    let chanStr = FIO.Channel<string>()
+open FSharp.FIO
 
-    let result = FIO.NaiveEval(Ring.processRing 20 2)
-    printfn $"Result: %A{result}"
+module Pingpong =
     
-    0
+    let intPing chanInt =
+        let x = 10
+        Send(x, chanInt, fun () ->
+            printfn $"intPing sent: %A{x}"
+            Receive(chanInt, fun y ->
+                printfn $"intPing received: %A{y}"
+                Send(y, chanInt, fun () ->
+                    printfn $"intPing sent: %A{y}"
+                    Receive(chanInt, fun z ->
+                        printfn $"intPing received: %A{z}"
+                        Return z))))
+    
+    let intPong chanInt =
+        Receive(chanInt, fun x ->
+            printfn $"intPong received: %A{x}"
+            let y = x + 10
+            Send(y, chanInt, fun () ->
+                printfn $"intPong sent: %A{y}"
+                Receive(chanInt, fun z ->
+                    printfn $"intPong received: %A{z}"
+                    let v = z + 10
+                    Send(v, chanInt, fun () ->
+                        printfn $"intPong sent: %A{v}"
+                        Return v))))
+    
+    let rec intPingInf chanInt =
+        let x = 10
+        Send(x, chanInt, fun () ->
+            printfn $"intPing sent: %A{x}"
+            Receive(chanInt, fun y ->
+                printfn $"intPing received: %A{y}"
+                intPingInf chanInt))
+    
+    let rec intPongInf chanInt =
+        Receive(chanInt, fun x ->
+            printfn $"intPong received: %A{x}"
+            let y = x + 10
+            Send(y, chanInt, fun () ->
+                printfn $"intPong sent: %A{y}"
+                intPongInf chanInt))
+    
+    let strPing chanStr =
+        let x = ""
+        Send(x, chanStr, fun () ->
+            printfn $"strPing sent: %A{x}"
+            Receive(chanStr, fun y ->
+                printfn $"strPing received: %A{y}"
+                Send(y, chanStr, fun () ->
+                    printfn $"strPing sent: %A{y}"
+                    Receive(chanStr, fun z ->
+                        printfn $"strPing received: %A{z}"
+                        Return z))))
+    
+    let strPong chanStr =
+        Receive(chanStr, fun x ->
+            printfn $"strPong received: %A{x}"
+            let y = x + "a"
+            Send(y, chanStr, fun () ->
+                printfn $"strPong sent: %A{y}"
+                Receive(chanStr, fun z ->
+                    printfn $"strPong received: %A{z}"
+                    let v = z + "b"
+                    Send(v, chanStr, fun () ->
+                        printfn $"strPong sent: %A{v}"
+                        Return v))))
+    
+    let rec strPingInf chanStr =
+        let x = ""
+        Send(x, chanStr, fun () ->
+            printfn $"strPing sent: %A{x}"
+            Receive(chanStr, fun y ->
+                printfn $"strPing received: %A{y}"
+                strPingInf chanStr))
+    
+    let rec strPongInf chanStr =
+        Receive(chanStr, fun x ->
+            printfn $"strPong received: %A{x}"
+            let y = x + "a"
+            Send(y, chanStr, fun () ->
+                printfn $"strPong sent: %A{y}"
+                strPongInf chanStr))
+    
+    let intPingpong chanInt =
+        Parallel(intPing chanInt, intPong chanInt)
+    
+    let intPingpongInf chanInt =
+        Parallel(intPingInf chanInt, intPongInf chanInt)
+    
+    let strPingpong chanStr =
+        Parallel(strPing chanStr, strPong chanStr)
+    
+    let strPingpongInf chanStr =
+        Parallel(strPingInf chanStr, strPongInf chanStr)
+    
+    let intStrPingpong chanInt chanStr =
+        Parallel(intPingpong chanInt, strPingpong chanStr)
+    
+    let intStrPingpongInf chanInt chanStr =
+        Parallel(intPingpongInf chanInt, strPingpongInf chanStr)
+
+module Ring = 
+
+    type private Process =
+        { Name: string
+          ChanSend: Channel<int>
+          ChanRecv: Channel<int>
+        }
+
+    let private createSendProcess chanSend chanRecv value name m =
+        let rec create n = 
+            if n = 1 then
+                Send(value, chanSend, fun () ->
+                    printfn $"%s{name} sent: %A{value}"
+                    Receive(chanRecv, fun v ->
+                        printfn $"%s{name} received: %A{v}"
+                        Return 0))
+            else 
+                Send(value, chanSend, fun () ->
+                    printfn $"%s{name} sent: %A{value}"
+                    Receive(chanRecv, fun v ->
+                        printfn $"%s{name} received: %A{v}"
+                        create (n - 1)))
+        create m
+
+    let private createRecvProcess chanRecv chanSend name m =
+        let rec create n =
+            if n = 1 then
+                Receive(chanRecv, fun v ->
+                    printfn $"%s{name} received: %A{v}"
+                    let value = v + 10
+                    Send(value, chanSend, fun () ->
+                        printfn $"%s{name} sent: %A{value}"
+                        Return 0))
+            else
+                Receive(chanRecv, fun v ->
+                         printfn $"%s{name} received: %A{v}"
+                         let value = v + 10
+                         Send(value, chanSend, fun () ->
+                             printfn $"%s{name} sent: %A{value}"
+                             create (n - 1)))
+        create m
+
+    let processRing processCount roundCount =
+        let par(effA, effB) = Concurrent(effA, fun asyncA ->
+                                  Concurrent(effB, fun asyncB ->
+                                      Await(asyncA, fun _ ->
+                                          Await(asyncB, fun _ ->
+                                              Return 0))))
+
+        let getRecvChan index (chans : Channel<int> list) =
+            match index with
+            | i when i - 1 < 0 -> chans.Item (List.length chans - 1)
+            | i                -> chans.Item (i - 1)
+
+        let rec createProcesses chans allChans index acc =
+            match chans with
+            | []    -> acc
+            | c::cs -> let proc = {Name = $"p{index}"; ChanSend = c; ChanRecv = getRecvChan index allChans}
+                       createProcesses cs allChans (index + 1) (acc @ [proc])
+
+        let rec createProcessRing procs index m = 
+            match procs with
+            | pa::pb::[] when index = 0 -> par(createSendProcess pa.ChanSend pa.ChanRecv 0 pa.Name m, createRecvProcess pb.ChanRecv pb.ChanSend pb.Name m)
+            | pa::pb::[]                -> par(createRecvProcess pa.ChanRecv pa.ChanSend pa.Name m, createRecvProcess pb.ChanRecv pb.ChanSend pb.Name m)
+            | p::ps when index = 0      -> par(createSendProcess p.ChanSend p.ChanRecv 0 p.Name m, createProcessRing ps (index + 1) m)
+            | p::ps                     -> par(createRecvProcess p.ChanRecv p.ChanSend p.Name m, createProcessRing ps (index + 1) m)
+            | _                         -> failwith "createRing failed!"
+
+        let chans = [for _ in 1..processCount -> Channel<int>()]
+
+        let processes = createProcesses chans chans 0 []
+
+        createProcessRing processes 0 roundCount
