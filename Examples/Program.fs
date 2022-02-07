a3e2f1ae838b83f19f0afe988b6853a53a169ca0
iyyel <mail@iyyel.io>
2022-02-07 15:53:41 +0100

    Small refactoring.

--- Examples/Program.fs
+++ Examples/Program.fs
@@ -8,27 +8,12 @@ open FSharp.FIO
 open Examples
 open System.Threading
 
-ThreadPool.SetMaxThreads(10000, 10000) |> ignore
+ThreadPool.SetMaxThreads(32767, 32767) |> ignore
 ThreadPool.SetMinThreads(10000, 10000) |> ignore
-
-let printThreadPoolInfo =
-    let maxWorkerThreads = ref 0;
-    let maxCompletePortThreads = ref 0;
-    let minWorkerThreads = ref 0;
-    let minCompletePortThreads = ref 0;
-    let avlWorkerThreads = ref 0;
-    let avlIoThreads = ref 0;
-    ThreadPool.GetMaxThreads(maxWorkerThreads, maxCompletePortThreads)
-    ThreadPool.GetMinThreads(minWorkerThreads, minCompletePortThreads)
-    ThreadPool.GetAvailableThreads(avlWorkerThreads, avlIoThreads);
-    printfn $"Thread pool information: "
-    printfn $"  Maximum worker threads: %A{maxWorkerThreads},\n  Maximum completion port threads %A{maxCompletePortThreads}"
-    printfn $"  Minimum worker threads: %A{minWorkerThreads},\n  Minimum completion port threads %A{minCompletePortThreads}"
-    printfn $"  Available worker threads: %A{avlWorkerThreads},\n  Available I/O threads: %A{avlIoThreads}"
 
 [<EntryPoint>]
 let main _ =
-    let result = NaiveEval <| Ring.processRing 5000 5000
+    let result = Runtime.NaiveRun <| Ring.processRing 10000 1
     printfn $"Result: %A{result}"
 
     0
--- FIO/FIO.fs
+++ FIO/FIO.fs
@@ -6,6 +6,7 @@ module FSharp.FIO
 
 open System.Collections.Concurrent
 open System.Threading.Tasks
+open System.Threading
 
 type Channel<'Msg>() =
     let bc = new BlockingCollection<'Msg>()
@@ -22,34 +23,34 @@ and [<AbstractClass>] FIO<'Success>() =
     abstract Visit<'Success> : FIOVisitor -> 'Success
 and Input<'Msg, 'Success>(chan : Channel<'Msg>, cont : 'Msg -> FIO<'Success>) =
     inherit FIO<'Success>()
-    member internal this.Chan = chan
-    member internal this.Cont = cont
-    override this.Visit<'Success>(input) =
-        input.VisitInput<'Msg, 'Success>(this)
+    member internal _.Chan = chan
+    member internal _.Cont = cont
+    override this.Visit<'Success>(visitor) =
+        visitor.VisitInput<'Msg, 'Success>(this)
 and Output<'Msg, 'Success>(value : 'Msg, chan : Channel<'Msg>, cont : unit -> FIO<'Success>) =
     inherit FIO<'Success>()
-    member internal this.Value = value
-    member internal this.Chan = chan
-    member internal this.Cont = cont
-    override this.Visit<'Success>(input) =
-        input.VisitOutput<'Msg, 'Success>(this)
+    member internal _.Value = value
+    member internal _.Chan = chan
+    member internal _.Cont = cont
+    override this.Visit<'Success>(visitor) =
+        visitor.VisitOutput<'Msg, 'Success>(this)
 and Concurrent<'Task, 'Success>(eff : FIO<'Task>, cont : Task<'Task> -> FIO<'Success>) =
     inherit FIO<'Success>()
-    member internal this.Eff = eff
-    member internal this.Cont = cont
-    override this.Visit<'Success>(con) =
-        con.VisitConcurrent<'Task, 'Success>(this)
+    member internal _.Eff = eff
+    member internal _.Cont = cont
+    override this.Visit<'Success>(visitor) =
+        visitor.VisitConcurrent<'Task, 'Success>(this)
 and Await<'Task, 'Success>(task : Task<'Task>, cont : 'Task -> FIO<'Success>) =
     inherit FIO<'Success>()
-    member internal this.Task = task
-    member internal this.Cont = cont
-    override this.Visit<'Success>(await) =
-        await.VisitAwait<'Task, 'Success>(this)
+    member internal _.Task = task
+    member internal _.Cont = cont
+    override this.Visit<'Success>(visitor) =
+        visitor.VisitAwait<'Task, 'Success>(this)
 and Succeed<'Success>(value : 'Success) =
     inherit FIO<'Success>()
-    member internal this.Value = value
-    override this.Visit<'Success>(input) =
-        input.VisitSucceed<'Success>(this)
+    member internal _.Value = value
+    override this.Visit<'Success>(visitor) =
+        visitor.VisitSucceed<'Success>(this)
 
 let Send<'Msg, 'Success>(value : 'Msg, chan : Channel<'Msg>, cont : (unit -> FIO<'Success>)) : Output<'Msg, 'Success> = Output(value, chan, cont)
 let Receive<'Msg, 'Success>(chan : Channel<'Msg>, cont : ('Msg -> FIO<'Success>)) : Input<'Msg, 'Success> = Input(chan, cont)
@@ -61,20 +62,40 @@ let Parallel<'SuccessA, 'SuccessB, 'SuccessC>(effA : FIO<'SuccessA>, effB : FIO<
                     cont (succA, succB)))))
 let End() : Succeed<unit> = Succeed ()
 
-let rec NaiveEval<'Success> (eff : FIO<'Success>) : 'Success =
-    eff.Visit({ 
-        new FIOVisitor with
-            member _.VisitInput<'Msg, 'Success>(input : Input<'Msg, 'Success>) =
-                let value = input.Chan.Receive
-                NaiveEval <| input.Cont value
-            member _.VisitOutput<'Msg, 'Success>(output : Output<'Msg, 'Success>) =
-                output.Chan.Send output.Value
-                NaiveEval <| output.Cont ()
-            member _.VisitConcurrent(con) =
-                let task = Task.Factory.StartNew(fun () -> NaiveEval con.Eff)
-                NaiveEval <| con.Cont task
-            member _.VisitAwait(await) =
-                NaiveEval <| await.Cont await.Task.Result
-            member _.VisitSucceed<'Success>(succ : Succeed<'Success>) =
-                succ.Value
-    })
+module Runtime =
+
+    let rec NaiveRun<'Success> (eff : FIO<'Success>) : 'Success =
+        eff.Visit({ 
+            new FIOVisitor with
+                member _.VisitInput<'Msg, 'Success>(input : Input<'Msg, 'Success>) =
+                    let value = input.Chan.Receive
+                    NaiveRun <| input.Cont value
+                member _.VisitOutput<'Msg, 'Success>(output : Output<'Msg, 'Success>) =
+                    output.Chan.Send output.Value
+                    NaiveRun <| output.Cont ()
+                member _.VisitConcurrent(con) =
+                    let task = Task.Factory.StartNew(fun () -> NaiveRun con.Eff)
+                    NaiveRun <| con.Cont task
+                member _.VisitAwait(await) =
+                    NaiveRun <| await.Cont await.Task.Result
+                member _.VisitSucceed<'Success>(succ : Succeed<'Success>) =
+                    succ.Value
+        })
+
+    let PrintThreadPoolInfo() =
+        let maxWorkerThreads = ref 0;
+        let maxCompletePortThreads = ref 0;
+        let minWorkerThreads = ref 0;
+        let minCompletePortThreads = ref 0;
+        let avlWorkerThreads = ref 0;
+        let avlIoThreads = ref 0;
+        ThreadPool.GetMaxThreads(maxWorkerThreads, maxCompletePortThreads)
+        ThreadPool.GetMinThreads(minWorkerThreads, minCompletePortThreads)
+        ThreadPool.GetAvailableThreads(avlWorkerThreads, avlIoThreads);
+        printfn $"Thread pool information: "
+        printfn $"Maximum worker threads: %A{maxWorkerThreads}"
+        printfn $"Maximum completion port threads %A{maxCompletePortThreads}"
+        printfn $"Minimum worker threads: %A{minWorkerThreads}"
+        printfn $"Minimum completion port threads %A{minCompletePortThreads}"
+        printfn $"Available worker threads: %A{avlWorkerThreads}"
+        printfn $"Available I/O threads: %A{avlIoThreads}"
