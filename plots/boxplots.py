01dc504876fff9dbcb9159a67b2b012dc0670ca4
iyyel <mail@iyyel.io>
2022-04-17 13:15:33 +0200

    Boxplots now satisfiable.

--- plots/boxplots.py
+++ plots/boxplots.py
@@ -1,42 +1,55 @@
 import data_loader as loader
+import matplotlib
+import matplotlib.ticker as ticker
 from matplotlib import pyplot as plt
 
-plt.style.use(['science', 'pgf'])
+"""
+matplotlib.use("pgf")
+matplotlib.rcParams.update({
+    "pgf.texsystem": "pdflatex",
+    'font.family': 'serif',
+    'text.usetex': True,
+    'pgf.rcfonts': False,
+})
+"""
 
-def save_individual():
-    for runtime_data in loader.all_runtime_data:
-        runtime_name_key = list(runtime_data.keys())[0]
-        runtime_data = runtime_data[runtime_name_key]
 
-        for benchmark_name_key in runtime_data.keys():
-            benchmark_data = runtime_data.get(benchmark_name_key)
-        
-            fig, ax = plt.subplots()
-            ax.boxplot(benchmark_data)
-            ax.set_xticks([1], [benchmark_name_key])
-            ax.set_ylabel('Time (ms)')
+plt.locator_params(axis="y", nbins=1)
 
-            runtime_name = runtime_name_key.lower().replace(' ', '-').replace(':', '')
-            benchmark_name = benchmark_name_key.lower().replace(' ', '-').replace(':', '')
-            file_name =  benchmark_name + '-' + runtime_name + '-boxplot.png'
-            plt.tight_layout()
-            plt.savefig('plots/boxplots/' + file_name)
-            #plt.show()
+for runtime_data in loader.all_runtime_data:
+    fig, axd = plt.subplot_mosaic([['topleft', 'topright'],['midleft', 'midright'],['bottom', 'bottom']])
+    for key in runtime_data.keys():
+        data = runtime_data[key]
+        # naive
+        naive_data = data[loader.pingpongKey]
+        axd['topleft'].boxplot(naive_data)
+        axd['topleft'].tick_params(axis='both', labelsize=12)
+        axd['topleft'].set_xticks([1], ['Pingpong'])
+        axd['topleft'].set_ylabel('Time (ms)', fontsize=12)
 
-def save_in_same_plot():
-    for runtime_data in loader.all_runtime_data:
-        runtime_name_key = list(runtime_data.keys())[0]
-        runtime_data = runtime_data[runtime_name_key]
-    
-        fig, ax = plt.subplots()
-        ax.boxplot(runtime_data.values())
-        ax.set_xticklabels(runtime_data.keys())
-        ax.set_ylabel('Time (ms)')
+        axd['topright'].boxplot(data[loader.threadringKey])
+        axd['topright'].tick_params(axis='both', labelsize=12)
+        axd['topright'].set_xticks([1], ['Threadring'])
+        axd['topright'].set_ylabel('Time (ms)', fontsize=12)
 
-        runtime_name = runtime_name_key.lower().replace(' ', '-').replace(':', '')
-        file_name = runtime_name + '-boxplot.png'
+        axd['midleft'].boxplot(data[loader.bigKey])
+        axd['midleft'].tick_params(axis='both', labelsize=12)
+        axd['midleft'].set_xticks([1], ['Big'])
+        axd['midleft'].set_ylabel('Time (ms)', fontsize=12)
+
+        axd['midright'].boxplot(data[loader.bangKey])
+        axd['midright'].tick_params(axis='both', labelsize=12)
+        axd['midright'].set_xticks([1], ['Bang'])
+        axd['midright'].set_ylabel('Time (ms)', fontsize=12)
+
+        axd['bottom'].boxplot(data[loader.reverseBangKey])
+        axd['bottom'].tick_params(axis='both', labelsize=12)
+        axd['bottom'].set_xticks([1], ['Reversebang'])
+        axd['bottom'].set_ylabel('Time (ms)', fontsize=12)
+
+        for ax in axd.values():
+            ax.yaxis.set_major_locator(ticker.MaxNLocator(nbins=4, min_n_ticks=4))
+
         plt.tight_layout()
-        #plt.savefig('plots/boxplots/' + file_name)
-        plt.show()
-
-save_in_same_plot()
+        plt.savefig('plots/boxplots/' + key + '-boxplot.png', bbox_inches='tight')
+       
